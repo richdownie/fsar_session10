6d30e73b10b272fe2434973f5ba77bab06331659
Rich Downie <rich@blackboxd.com>
2019-11-29 11:15:06 -0500

    updates

--- features/support/env.rb
+++ features/support/env.rb
@@ -6,7 +6,6 @@ require 'pry'
 require_relative '../page_objects/widgets_index_page'
 require_relative '../page_objects/widget_new_page'
 
-
 options = Selenium::WebDriver::Chrome::Options.new(args: ['--headless'])
 
 Before do
